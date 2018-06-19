f9b43a3d88ab65e312b4c3e9cdce20a6cab11c9e
RangerOfAeon <is1997@hotmail.com>
2018-06-19 16:00:55 +0200

    Balbla

--- MvcGround/Controllers/PersonController.cs
+++ MvcGround/Controllers/PersonController.cs
@@ -10,42 +10,42 @@ namespace MvcGround.Controllers
     public class PersonController : Controller
     {
         public static int Start;
-
+        //public model = new PeopleViewModel();
+        //public model.People = new List<ViewModels>();
 
         // GET: Person
         //[HttpGet]
         public ActionResult ViewPeople(string searchTerm = null)
         {
 
-            var model = new PeopleViewModel();
-            model.People = new List<ViewModels>();
+            //var model = new PeopleViewModel();
+            //model.People = new List<ViewModels>();
 
             //model.People.Add(new ViewModels { Name = "Isaac", PhoneNumber = "0723955431", City = "Skara" });
             //model.People.Add(new ViewModels { Name = "Thomas", PhoneNumber = "0721234567", City = "Skara" });
             //model.People.Add(new ViewModels { Name = "Elliot", PhoneNumber = "0729876543", City = "Skara" });
             //model.People.Add(new ViewModels { Name = "Linda", PhoneNumber = "0721239876", City = "Alingsås" });
 
-            model.People
-                .OrderBy(r => r.Name)
-                .Where(r => searchTerm == null || (r.Name.Contains(searchTerm) || r.City.Contains(searchTerm)))
-                .Take(10)
-                .Select(r => r);
+
             //where r => searchTerm == null || r.Name.StartsWithsearchTerm
             //select r;
 
-            
+            model.People
+              .OrderBy(r => r.Name)
+              .Where(r => searchTerm == null || (r.Name.Contains(searchTerm) || r.City.Contains(searchTerm)))
+              .Select(r => r);
 
             Start++;
             if (Start == 1)
             {
-                model.People.Add(new ViewModels { Name = "Isaac", PhoneNumber = "0723955431", City = "Skara" });
-                model.People.Add(new ViewModels { Name = "Thomas", PhoneNumber = "0721234567", City = "Skara" });
-                model.People.Add(new ViewModels { Name = "Elliot", PhoneNumber = "0729876543", City = "Skara" });
-                model.People.Add(new ViewModels { Name = "Linda", PhoneNumber = "0721239876", City = "Alingsås" });
+                CurrentPeople.Add(new ViewModels { Name = "Isaac", PhoneNumber = "0723955431", City = "Skara" });
+                CurrentPeople.Add(new ViewModels { Name = "Thomas", PhoneNumber = "0721234567", City = "Skara" });
+                CurrentPeople.Add(new ViewModels { Name = "Elliot", PhoneNumber = "0729876543", City = "Skara" });
+                CurrentPeople.Add(new ViewModels { Name = "Linda", PhoneNumber = "0721239876", City = "Alingsås" });
             }
 
-                //}
-                if (Request.IsAjaxRequest())
+            //}
+            if (Request.IsAjaxRequest())
             {
                 return PartialView("_PartialPerson", model);
             }
@@ -59,31 +59,40 @@ namespace MvcGround.Controllers
         }
         public ActionResult Delete(string id)
         {
-            var model = new PeopleViewModel();
-            model.People = new List<ViewModels>();
+            //var model = new PeopleViewModel();
+            //model.People = new List<ViewModels>();
 
-            ViewModels match = model.People.Find(r => r.Name == id);
-            if (match != null && model.People.Contains(match))
+            ViewModels match = CurrentPeople.Find(r => r.Name == id);
+            if (match != null && CurrentPeople.Contains(match))
             {
-                model.People.Remove(match);
+                CurrentPeople.Remove(match);
             }
-            return PartialView("_PartialPerson", model.People);
+            return PartialView("_PartalPerson", CurrentPeople);
             //return View();
         }
         //public ActionResult Edit(string id)
         //{
         //    var model = 
         //}
-        //[HttpPost]
-        //public ActionResult Delete(int id, FormCollection collection)
-        //{
-        //    return RedirectToAction("ViewPeople", PeopleViewModel.People);
-        //}
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            //var model = new PeopleViewModel();
+            //model.People = new List<ViewModels>();
+            try
+            {
+                return PartialView("_PartialPerson");
+            }
+            catch
+            {
+                return View();
+            }
+        }
         [HttpPost] 
         public ActionResult Create(ViewModels person)
         {
-            var model = new PeopleViewModel();
-            model.People = new List<ViewModels>();
+            //var model = new PeopleViewModel();
+            //model.People = new List<ViewModels>();
 
             try
             {
@@ -91,10 +100,10 @@ namespace MvcGround.Controllers
                 {
                     if (ModelState.IsValid)
                     {
-                        model.People.Add(new ViewModels { Name = person.Name, PhoneNumber = person.PhoneNumber, City = person.City });
+                        CurrentPeople.Add(new ViewModels { Name = person.Name, PhoneNumber = person.PhoneNumber, City = person.City });
                     }
                 }
-                return PartialView("_PartialPerson");
+                return RedirectToAction("ViewPeople", CurrentPeople);
             }
             catch
             {
@@ -103,5 +112,13 @@ namespace MvcGround.Controllers
            
             
         }
+        public PeopleViewModel model = new PeopleViewModel();
+        public static List<ViewModels> CurrentPeople = new List<ViewModels>
+        {
+            new ViewModels { Name = "Isaac", PhoneNumber = "0723955431", City = "Skara" },
+            new ViewModels { Name = "Thomas", PhoneNumber = "0721234567", City = "Skara" },
+            new ViewModels { Name = "Elliot", PhoneNumber = "0729876543", City = "Skara" },
+            new ViewModels { Name = "Linda", PhoneNumber = "0721239876", City = "Alingsås" },
+        };
     }
 }
